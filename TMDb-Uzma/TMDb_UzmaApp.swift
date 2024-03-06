3289cd28fd6354c1478118511b53a88d26d67c78
Uzma Sayyed <uzmasayyed7@gmail.com>
2024-03-06 16:38:07 +0530

    Initial Commit

---
+++ TMDb-Uzma/TMDb_UzmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDb_UzmaApp.swift
+//  TMDb-Uzma
+//
+//  Created by Uzma Sayyed on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDb_UzmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TMDb-UzmaUITests/TMDb_UzmaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TMDb_UzmaUITestsLaunchTests.swift
+//  TMDb-UzmaUITests
+//
+//  Created by Uzma Sayyed on 06/03/24.
+//
+
+import XCTest
+
+final class TMDb_UzmaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
